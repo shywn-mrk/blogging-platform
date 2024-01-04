f9cea253795fe7338fde431bc854000f415a17d5
Shayan Karimi <shayankarimi0078@gmail.com>
2024-01-04 20:21:42 +0330

    Initial commit

---
+++ blogging_platform/conftest.py
@@ -0,0 +1,14 @@
+import pytest
+
+from blogging_platform.users.models import User
+from blogging_platform.users.tests.factories import UserFactory
+
+
+@pytest.fixture(autouse=True)
+def media_storage(settings, tmpdir):
+    settings.MEDIA_ROOT = tmpdir.strpath
+
+
+@pytest.fixture
+def user(db) -> User:
+    return UserFactory()
