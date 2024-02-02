430d3b140fcf0568e51c6e9b92e84bda5a1b8c77
Shayan Karimi <shayankarimi0078@gmail.com>
2024-02-02 22:03:38 +0330

    Add redis cache

--- blogging_platform/posts/api/views.py
+++ blogging_platform/posts/api/views.py
@@ -1,24 +1,29 @@
+from django.utils.decorators import method_decorator
+from django.views.decorators.cache import cache_page
 from rest_framework import viewsets
 
+from blogging_platform.posts.api.permissions import IsBlogUserPermission
 from blogging_platform.posts.api.serializers import *
 from blogging_platform.posts.filters import PostFilterSet
 from blogging_platform.posts.models import *
 
 from .paginations import StandardResultsSetPagination
 
-# from blogging_platform.posts.api.permissions import IsBlogUserPermission
-
 
 class PostViewset(viewsets.ModelViewSet):
-    # queryset = Post.objects.all()
+    queryset = Post.objects.all()
     serializer_class = PostSerializer
-    # permission_classes = [IsBlogUserPermission]
+    permission_classes = [IsBlogUserPermission]
     filterset_class = PostFilterSet
     pagination_class = StandardResultsSetPagination
     search_fields = ["title"]
 
     def get_queryset(self):
         return Post.objects.filter(user=self.request.user)  # type: ignore
+
+    @method_decorator(cache_page(60 * 60 * 2))
+    def list(self, request, *args, **kwargs):
+        return super().list(request, *args, **kwargs)
 
     def perform_create(self, serializer) -> None:
         serializer.save(user=self.request.user)
